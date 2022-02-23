d9bdb295ff24ce0da94a6a34ba20cf9b4dd40fd4
Movind26 <movind26@protonmail.ch>
2022-02-23 12:41:36 +0300

    Adding range for 2nd argument check

--- task1.js
+++ task1.js
@@ -3,6 +3,8 @@ let secondValue = +prompt('Enter second value', 0);
 
 if (isNaN(firstValue) || isNaN(secondValue) || !firstValue || !secondValue) {
   console.log('Input error!');
-} else {
-  console.log(firstValue.toString(secondValue));
-}
+} else if (secondValue < 2 || secondValue > 36) {
+    console.log('Second argument must be between 2 and 36. Try again.');
+  } else {
+      console.log(firstValue.toString(secondValue));
+    }
