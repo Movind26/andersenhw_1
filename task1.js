26a922a598cebd9e23454523039e8218855cb321
Movind26 <movind26@protonmail.ch>
2022-02-24 13:11:38 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -2,9 +2,9 @@ const firstValue  = +prompt('Enter first value' , 0);
 const secondValue = +prompt('Enter second value', 0);
 
 if (isNaN(firstValue) || isNaN(secondValue) || !firstValue || !secondValue) {
-  'Input error!';
+  console.log('Input error!');
 } else if (secondValue < 2 || secondValue > 36) {
-  'Second argument must be between 2 and 36. Try again.';
+  console.log('Second argument must be between 2 and 36. Try again.');
 } else {
-  firstValue.toString(secondValue);
+  console.log(firstValue.toString(secondValue));
 }
